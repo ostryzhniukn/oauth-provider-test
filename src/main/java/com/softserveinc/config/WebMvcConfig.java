05807e99bfc8333668a6deaafe464c8bdd003e57
Nazar <nazarostryzhniuk@gmail.com>
2015-03-27 14:20:26 +0200

    JSP support enabled

--- src/main/java/com/softserveinc/config/WebMvcConfig.java
+++ src/main/java/com/softserveinc/config/WebMvcConfig.java
@@ -1,69 +1,69 @@
 package com.softserveinc.config;
-//
-//import java.util.Arrays;
-//
-//import org.apache.ibatis.session.SqlSessionFactory;
-//import org.apache.log4j.Logger;
-//import org.mybatis.spring.SqlSessionFactoryBean;
-//import org.mybatis.spring.SqlSessionTemplate;
-//import org.mybatis.spring.mapper.MapperScannerConfigurer;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-//import org.springframework.core.io.ClassPathResource;
-//import org.springframework.core.io.Resource;
-//import org.springframework.http.MediaType;
-//import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-//import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-//import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-//import org.springframework.transaction.annotation.EnableTransactionManagement;
-//import org.springframework.web.accept.ContentNegotiationManagerFactoryBean;
-//import org.springframework.web.servlet.View;
-//import org.springframework.web.servlet.ViewResolver;
-//import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-//import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
-//import org.springframework.web.servlet.view.InternalResourceViewResolver;
-//import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
-//
+
+import java.util.Arrays;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.log4j.Logger;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.accept.ContentNegotiationManagerFactoryBean;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
 //import javax.sql.DataSource;
 //
-//@Configuration
+@Configuration
 //@ComponentScan(basePackages = {"com.softserveinc"})
-//@EnableWebMvc
-//public class WebMvcConfig extends WebMvcConfigurerAdapter {
-//
-//	@Bean
-//	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-//		return new PropertySourcesPlaceholderConfigurer();
-//	}
-//
-//	@Bean
-//	public ContentNegotiatingViewResolver contentViewResolver() throws Exception {
-//		ContentNegotiationManagerFactoryBean contentNegotiationManager = new ContentNegotiationManagerFactoryBean();
-//		contentNegotiationManager.addMediaType("json", MediaType.APPLICATION_JSON);
-//
-//		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-//		viewResolver.setPrefix("/WEB-INF/jsp/");
-//		viewResolver.setSuffix(".jsp");
-//
-//		MappingJackson2JsonView defaultView = new MappingJackson2JsonView();
-//		defaultView.setExtractValueFromSingleKeyModel(true);
-//
-//		ContentNegotiatingViewResolver contentViewResolver = new ContentNegotiatingViewResolver();
-//		contentViewResolver.setContentNegotiationManager(contentNegotiationManager.getObject());
-//		contentViewResolver.setViewResolvers(Arrays.<ViewResolver> asList(viewResolver));
-//		contentViewResolver.setDefaultViews(Arrays.<View> asList(defaultView));
-//		return contentViewResolver;
-//	}
-//
-//	@Override
-//	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-//		configurer.enable();
-//	}
+@EnableWebMvc
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+
+	@Bean
+	public ContentNegotiatingViewResolver contentViewResolver() throws Exception {
+		ContentNegotiationManagerFactoryBean contentNegotiationManager = new ContentNegotiationManagerFactoryBean();
+		contentNegotiationManager.addMediaType("json", MediaType.APPLICATION_JSON);
+
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/jsp/");
+		viewResolver.setSuffix(".jsp");
+
+		MappingJackson2JsonView defaultView = new MappingJackson2JsonView();
+		defaultView.setExtractValueFromSingleKeyModel(true);
+
+		ContentNegotiatingViewResolver contentViewResolver = new ContentNegotiatingViewResolver();
+		contentViewResolver.setContentNegotiationManager(contentNegotiationManager.getObject());
+		contentViewResolver.setViewResolvers(Arrays.<ViewResolver> asList(viewResolver));
+		contentViewResolver.setDefaultViews(Arrays.<View> asList(defaultView));
+		return contentViewResolver;
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
 //
 //    //@Configuration
 //    @EnableTransactionManagement
@@ -132,4 +132,4 @@ package com.softserveinc.config;
 //            return null;
 //        }
 //    }
-//}
+}
